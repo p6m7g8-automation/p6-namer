d2aa2580fdad455a2e32091b073d4a7fe263f857
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-05 19:06:05 -0500

    chore: enable may features (#591)
    * chore: enable may features
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,12 @@
-import { awscdk } from "projen";
+import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
+  name: 'p6-namer',
   author: 'Philip M. Gollucci',
   authorAddress: 'pgollucci@p6m7g8.com',
-  cdkVersion: '2.15.0',
-  defaultReleaseBranch: 'main',
-  name: 'p6-namer',
+  authorOrganization: true,
   repositoryUrl: 'https://github.com/p6m7g8/p6-namer.git',
   description: 'Sets the AWS IAM Account Alias with a Custom Resource',
+  stability: 'stable',
   keywords: [
     'aws',
     'cdk',
@@ -16,8 +16,18 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'landing zone',
   ],
 
+  cdkVersion: '2.15.0',
+  defaultReleaseBranch: 'main',
   projenrcTs: true,
   gitpod: true,
+  devContainer: true,
+  codeCov: true,
+  prettier: true,
+  releaseFailureIssue: true,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ['p6m7g8-automation'],
+  },
 
   peerDeps: [
     'aws-cdk-lib',
--- test/p6-namer.test.ts
+++ test/p6-namer.test.ts
@@ -1,25 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
 
-import { P6Namer } from '../src';
+import { P6Namer } from "../src";
 
-test('P6Namer components', () => {
+test("P6Namer components", () => {
   // GIVEN
   const app = new cdk.App();
-  const stack = new cdk.Stack(app, 'MyStack');
+  const stack = new cdk.Stack(app, "MyStack");
 
   // WHEN
-  new P6Namer(stack, 'p6-namer', {
-    accountAlias: 'theAlias',
+  new P6Namer(stack, "p6-namer", {
+    accountAlias: "theAlias",
   });
 
   // THEN
   const template = Template.fromStack(stack);
-  template.hasResourceProperties('AWS::Lambda::Function', {
-    Handler: 'index.handler',
-    Runtime: 'nodejs14.x',
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Handler: "index.handler",
+    Runtime: "nodejs14.x",
   });
-  template.resourceCountIs('AWS::Lambda::Function', 2); // Custom Resource Handler counts too
-
+  template.resourceCountIs("AWS::Lambda::Function", 2); // Custom Resource Handler counts too
 });
-
